9003f0d54e6f7817f02236808b7631cf685cc7de
Bryan Hunter <bdhunter3141@gmail.com>
2019-06-01 00:38:43 -0400

    users now able to sign in and link their facebook and google accounts.

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -16,8 +16,11 @@ class Firebase {
   constructor() {
     app.initializeApp(config);
 
+    this.emailAuthProvider = app.auth.EmailAuthProvider;
     this.auth = app.auth();
     this.db = app.firestore();
+    this.googleProvider = new app.auth.GoogleAuthProvider();
+    this.facebookProvider = new app.auth.FacebookAuthProvider();
   }
 
   // *** Auth API ***
@@ -27,6 +30,12 @@ class Firebase {
 
   doSignInWithEmailAndPassword = (email, password) =>
     this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignInWithGoogle = () =>
+    this.auth.signInWithPopup(this.googleProvider);
+
+  doSignInWithFacebook = () =>
+    this.auth.signInWithPopup(this.facebookProvider);
 
   doSignOut = () => this.auth.signOut();
 
