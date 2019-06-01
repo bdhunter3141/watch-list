b60ecc61548fbf582e97533901c459caf0c5c01f
Bryan Hunter <bdhunter3141@gmail.com>
2019-06-01 19:38:41 -0400

    added individual user page to admin with password reset link.

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import { compose } from "recompose";
 
 import { withFirebase } from "../Firebase";
-import { withAuthorization, withEmailVerification } from '../Session';
+import { withAuthorization, withEmailVerification } from "../Session";
 import * as ROLES from "../../constants/roles";
+import * as ROUTES from "../../constants/routes";
 
-class AdminPage extends Component {
+const AdminPage = () => (
+  <div>
+    <h1>Admin</h1>
+    <p>The Admin Page is accessible by every signed in admin user.</p>
+    <Switch>
+      <Route exact path={ROUTES.ADMIN_DETAILS} component={UserItem} />
+      <Route exact path={ROUTES.ADMIN} component={UserList} />
+    </Switch>
+  </div>
+);
+
+class UserListBase extends Component {
   state = {
     loading: false,
     users: []
@@ -35,39 +48,106 @@ class AdminPage extends Component {
 
     return (
       <div>
-        <h1>Admin</h1>
-
-        <p>The Admin Page is accessible by every signed in admin user.</p>
+        <h2>Users</h2>
 
         {loading && <div>Loading ...</div>}
 
-        <UserList users={users} />
+        <ul>
+          {users.map(user => (
+            <li key={user.uid}>
+              <span>
+                <strong>ID:</strong> {user.uid}
+              </span>
+              <span>
+                <strong>E-Mail:</strong> {user.email}
+              </span>
+              <span>
+                <strong>Username:</strong> {user.username}
+              </span>
+              <span>
+                <Link
+                  to={{
+                    pathname: `${ROUTES.ADMIN}/users/${user.uid}`,
+                    state: { user }
+                  }}
+                >
+                  Details
+                </Link>
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
-const UserList = ({ users }) => (
-  <ul>
-    {users.map(user => (
-      <li key={user.uid}>
-        <span>
-          <strong>ID:</strong> {user.uid}
-        </span>
-        <span>
-          <strong>E-Mail:</strong> {user.email}
-        </span>
-        <span>
-          <strong>Username:</strong> {user.username}
-        </span>
-      </li>
-    ))}
-  </ul>
-);
+class UserItemBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      user: null,
+      ...props.location.state
+    };
+  }
+
+  componentDidMount() {
+    if (this.state.user) {
+      return;
+    }
+    this.setState({ loading: true });
+
+    this.props.firebase
+      .user(this.props.match.params.id)
+      .get()
+      .then(querySnapshot => {
+        this.setState({
+          user: querySnapshot.data(),
+          loading: false
+        });
+      });
+  }
+
+  onSendPasswordResetEmail = () => {
+    this.props.firebase.doPasswordReset(this.state.user.email);
+  };
+
+  render() {
+    const { user, loading } = this.state;
+    return (
+      <div>
+        <h2>User ({this.props.match.params.id})</h2>
+        {loading && <div>Loading ...</div>}
+        {user && (
+          <div>
+            <span>
+              <strong>ID:</strong> {user.uid}
+            </span>
+            <span>
+              <strong>E-Mail:</strong> {user.email}
+            </span>
+            <span>
+              <strong>Username:</strong> {user.username}
+            </span>
+            <span>
+              <button type="button" onClick={this.onSendPasswordResetEmail}>
+                Send Password Reset
+              </button>
+            </span>
+          </div>
+        )}{" "}
+      </div>
+    );
+  }
+}
 
 const condition = authUser => authUser && !!authUser.roles[ROLES.ADMIN];
 
+const UserList = withFirebase(UserListBase);
+const UserItem = withFirebase(UserItemBase);
+
 export default compose(
-  withEmailVerification, withAuthorization(condition),
-  withFirebase
+  withEmailVerification,
+  withAuthorization(condition)
 )(AdminPage);
